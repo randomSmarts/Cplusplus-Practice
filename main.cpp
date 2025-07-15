e2304c48c9201f2278c27ccdf3992274472388e9
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2025-07-15 01:47:53 -0700

    Finished project 7.3?

--- main.cpp
+++ main.cpp
@@ -5,63 +5,46 @@ Date: 7/15/25
 Instructor: Dave Harden
 File: main.cpp
 
-This program reads a series of integers from the user and determines the
-positions of the first and last occurrences of the number 7. The user
-specifies how many numbers will be entered and then inputs each number.
-If the number 7 appears, the program reports the positions of its first
-and last appearances; otherwise, it displays a message that no 7 was '
-entered. All output is printed to the console.
+This program repeatedly prompts the user to enter the name of a text file and
+counts the number of words in that file. If the file opens successfully, the
+program displays the total word count; otherwise, it displays an error message.
+The user can continue entering filenames, and the program terminates when the
+user enters "quit". All results are printed to the console.
 */
 
 #include <iostream>
-#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
-constexpr int POSITION_NUMBER_CHOSEN = 7;
-constexpr int POSITION_CHECKER = -9999;
-
-const string FIRST_NUMBER_POSITION_P1 = "The first ";
-const string FIRST_NUMBER_POSITION_P2 = " was in position ";
-const string LAST_NUMBER_POSITION_P1 = "The last ";
-const string LAST_NUMBER_POSITION_P2 = " was in position ";
-const string NO_NUMBER_POSITION_P1 = "Sorry, no ";
-const string NO_NUMBER_POSITION_P2 = "'s were entered.";
-
 int main() {
-    int numbersEntered;
-    int userNum;
-    int firstPosition = POSITION_CHECKER;
-    int lastPosition = POSITION_CHECKER;
+    string userInput;
+    string currentWord;
+    ifstream inputFile;
+    int numberOfWords = 0;
 
-    cout << "How many numbers will be entered? ";
-    cin >> numbersEntered;
+    cout << "Enter the filename: ";
+    cin >> userInput;
 
-    for (int i = 0; i < numbersEntered; i++) {
-        cout << "Enter num: ";
-        cin >> userNum;
+    while (userInput != "quit") {
+        inputFile.open(userInput);
 
-        if (userNum == POSITION_NUMBER_CHOSEN &&
-            (firstPosition == POSITION_CHECKER || i < firstPosition)) {
-            firstPosition = i + 1;
+        if (inputFile) {
+            while (inputFile >> currentWord) {
+                numberOfWords += 1;
+            }
+
+            cout << "The file has " << numberOfWords << " words." << endl;
+        }
+        else {
+            cout << "Couldn't open file." << endl;
         }
 
-        if (userNum == POSITION_NUMBER_CHOSEN &&
-            (lastPosition == POSITION_CHECKER || i > lastPosition - 1)) {
-            lastPosition = i + 1;
-        }
-    }
+        numberOfWords = 0;
+        inputFile.close();
 
-    if (firstPosition == POSITION_CHECKER || lastPosition ==
-        POSITION_CHECKER) {
-        cout << NO_NUMBER_POSITION_P1 << POSITION_NUMBER_CHOSEN
-            << NO_NUMBER_POSITION_P2 << endl;
-    }
-    else {
-        cout << FIRST_NUMBER_POSITION_P1 << POSITION_NUMBER_CHOSEN
-            << FIRST_NUMBER_POSITION_P2 << firstPosition << endl;
-        cout << LAST_NUMBER_POSITION_P1 << POSITION_NUMBER_CHOSEN
-            << LAST_NUMBER_POSITION_P2 << lastPosition << endl;
+        cout << "Enter the filename: ";
+        cin >> userInput;
     }
 
     return 0;
