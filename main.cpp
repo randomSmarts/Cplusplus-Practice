2dc616de6ea6872aa4a505aa42fa1230aad10512
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2025-07-21 00:07:36 -0700

    restrucutred

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 /*
 Name: Aarsh Mittal
 Class: C S -002A-02W
-Date: 7/15/25
+Date: 7/21/25
 Instructor: Dave Harden
 File: main.cpp
 
-This program repeatedly prompts the user to enter the name of a text file and
+FIXME: This description is wrong This program repeatedly prompts the user to enter the name of a text file and
 counts the number of words in that file. If the file opens successfully, the
 program displays the total word count; otherwise, it displays an error message.
 The user can continue entering filenames, and the program terminates when the
@@ -18,34 +18,7 @@ user enters "quit". All results are printed to the console.
 using namespace std;
 
 int main() {
-    string userInput;
-    string currentWord;
-    ifstream inputFile;
-    int numberOfWords = 0;
 
-    cout << "Enter the filename: ";
-    cin >> userInput;
-
-    while (userInput != "quit") {
-        inputFile.open(userInput);
-
-        if (inputFile) {
-            while (inputFile >> currentWord) {
-                numberOfWords += 1;
-            }
-
-            cout << "The file has " << numberOfWords << " words." << endl;
-        }
-        else {
-            cout << "Couldn't open file." << endl;
-        }
-
-        numberOfWords = 0;
-        inputFile.close();
-
-        cout << "Enter the filename: ";
-        cin >> userInput;
-    }
 
     return 0;
 }
