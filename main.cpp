bd37ffcd14bd681af639d4b260f43a8c45b88d0c
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2025-08-01 23:42:25 -0700

    Working up Project 14.1

--- main.cpp
+++ main.cpp
@@ -17,13 +17,73 @@ for each set and overall.
 */
 
 #include <iostream>
-#include <iomanip>
-#include <fstream>
-
 using namespace std;
 
+class Fraction
+{
+public:
+    void set (int userNumerator, int userDenominator);
 
+private:
+    unsigned int numerator;
+    unsigned int denominator;
 
-int main() {
+};
 
+void Fraction::set (int userNumerator, int userDenominator)
+{
+    numerator = userNumerator;
+    denominator = userDenominator;
+}
+
+int main()
+{
+    Fraction f1;
+    Fraction f2;
+    Fraction result;
+
+    f1.set(9, 8);
+    f2.set(2, 3);
+
+    cout << "The product of ";
+    f1.print();
+    cout << " and ";
+    f2.print();
+    cout << " is ";
+    result = f1.multipliedBy(f2);
+    result.print();
+    cout << endl;
+
+    cout << "The quotient of ";
+    f1.print();
+    cout << " and ";
+    f2.print();
+    cout << " is ";
+    result = f1.dividedBy(f2);
+    result.print();
+    cout << endl;
+
+    cout << "The sum of ";
+    f1.print();
+    cout << " and ";
+    f2.print();
+    cout << " is ";
+    result = f1.addedTo(f2);
+    result.print();
+    cout << endl;
+
+    cout << "The difference of ";
+    f1.print();
+    cout << " and ";
+    f2.print();
+    cout << " is ";
+    result = f1.subtract(f2);
+    result.print();
+    cout << endl;
+
+    if (f1.isEqualTo(f2)){
+        cout << "The two Fractions are equal." << endl;
+    } else {
+        cout << "The two Fractions are not equal." << endl;
+    }
 }
