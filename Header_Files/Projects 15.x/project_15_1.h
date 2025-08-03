a0fe61d4eeb5f1b91c501eaaa19d17ff0545cdfa
randomSmarts <67401115+randomSmarts@users.noreply.github.com>
2025-08-02 23:17:30 -0700

    Set up Project 15.1

---
+++ Header_Files/Projects 15.x/project_15_1.h
@@ -0,0 +1,16 @@
+//
+// Created by Aarsh Mittal on 8/2/25.
+//
+
+#ifndef PROJECT_15_1_H
+#define PROJECT_15_1_H
+
+
+
+class project_15_1 {
+
+};
+
+
+
+#endif //PROJECT_15_1_H
---
+++ Projects/Projects 15.x/project_15_1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aarsh Mittal on 8/2/25.
+//
+
+#include "project_15_1.h"
